fad3871193e044fa8b0a92c52feff2f1e9e1bc78
Zach Weaver <r3dsierra@gmail.com>
2017-07-19 10:05:32 -0700

    add basic functional sketch pad

---
+++ js/etch_a_sketch.js
@@ -0,0 +1,36 @@
+
+var side = 16;
+var size = 48;
+
+
+$(document).ready(function() {
+  create();
+  reset();
+});
+
+
+function reset() {
+  $("button").click(function() {
+    $(".pixel").css("background-color", "#E84A47");
+    side = prompt("How many ballons per side would you like your grid to be? No more than 99 red ballons.");
+    size = 768/side
+    $(".container").empty();
+    create();
+  });
+}
+
+function hover() {
+  $(".pixel").hover(function() {
+    $(this).css("background-color", "pink");
+  });
+}
+
+function create() {
+  var total = side * side;
+  for(var i = 0;i < total; i++) {
+    $(".container").append("<div class='pixel'></div>");
+  }
+  $(".pixel").css("width", size);
+  $(".pixel").css("height", size);
+  hover();
+}
